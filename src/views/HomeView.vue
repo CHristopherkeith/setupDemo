c1c3ae5e0ff8932850307e506bc526cdee832daa
Christopher <easonmu@hotmail.com>
2022-12-29 23:49:46 +0800

    feat: 测试：添加小程序api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="onClick">click</button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import HomeView1 from '1030SetupNpmlink/src/views/HomeView.vue'
-import HomeView2 from '../../node_modules/1030SetupNpmlink/src/views/HomeView.vue'
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HomeView1 from "1030SetupNpmlink/src/views/HomeView.vue";
+import HomeView2 from "../../node_modules/1030SetupNpmlink/src/views/HomeView.vue";
 
-
-// export default defineComponent({
-//   name: 'HomeView',
-//   components: {
-//     HelloWorld,
-//   },
-// });
+function onClick(): void {
+  console.log(window.wx, "[window.wx]");
+  // window.wx.openLocation({ data: "foo" });
+  window.wx.miniProgram.postMessage({ data: {foo: 'bar'} });
+}
 </script>
