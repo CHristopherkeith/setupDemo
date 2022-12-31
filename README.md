045d4143306f9dddf6ba4b50670eb6ab0a2df56d
Christopher <easonmu@hotmail.com>
2022-12-31 20:51:51 +0800

    feat: 测试：添加url打开小程序页面

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### h5与小程序通信
+- `postMessage`，需要在小程序触发特定的行为（回退、组件销毁及分享）才能唤起小程序相应的方法
+- `navigateTo`，通过url传参，跳转到中转页面，可以触发相应小程序的方法
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="home">
     <button @click="onClick">click</button>
+    <input v-model="value" />
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Ref, ref } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import HomeView1 from "1030SetupNpmlink/src/views/HomeView.vue";
 import HomeView2 from "../../node_modules/1030SetupNpmlink/src/views/HomeView.vue";
 
+let value: Ref<string> = ref("");
+
 function onClick(): void {
   console.log(window.wx, "[window.wx]");
-  // window.wx.openLocation({ data: "foo" });
-  window.wx.miniProgram.postMessage({ data: {foo: 'bar'} });
+
+  /**
+   * @description postMessage
+   */
+
+  window.wx.miniProgram.postMessage({ data: { foo: "bar" } });
+
+  // window.wx.miniProgram.navigateBack();
+
+  /**
+   * @description navigateTo
+   */
+
+  // window.wx.miniProgram.navigateTo({url: '/pages/placeholder/placeholder'})
+
+  // window.wx.miniProgram.navigateBack({delta: 1})
+  // window.wx.miniProgram.redirectTo({
+  //   url: "/pages/index",
+  // });
 }
 </script>
