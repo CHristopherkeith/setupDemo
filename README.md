4e92afc631f7d89b7cfb4d8e9c4941042675056b
Christopher <easonmu@hotmail.com>
2023-01-02 22:00:07 +0800

    feat: 测试：h5与小程序通讯

--- README.md
+++ README.md
@@ -20,4 +20,7 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 ### h5与小程序通信
 - `postMessage`，需要在小程序触发特定的行为（回退、组件销毁及分享）才能唤起小程序相应的方法
-- `navigateTo`，通过url传参，跳转到中转页面，可以触发相应小程序的方法
+- `navigateTo`，通过`url`传参，跳转到中转页面，可以触发相应小程序的方法
+  1. 此方案的`hash`值污染的问题（路由拦截或在h5回退）
+  2. 小程序`pages`的通讯需要使用发布订阅机制吗？（考虑使用`getCurrentPages`）
+  3. 小程序`pages/index`是否有需要使用计算属性？
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,7 @@ function onClick(): void {
    * @description postMessage
    */
 
-  window.wx.miniProgram.postMessage({ data: { foo: "bar" } });
+  // window.wx.miniProgram.postMessage({ data: { foo: "bar" } });
 
   // window.wx.miniProgram.navigateBack();
 
@@ -30,7 +30,23 @@ function onClick(): void {
    * @description navigateTo
    */
 
-  // window.wx.miniProgram.navigateTo({url: '/pages/placeholder/placeholder'})
+  window.wx.miniProgram.navigateTo({
+    url: "/pages/placeholder/placeholder",
+    events: {
+      // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
+      acceptDataFromOpenedPage: function (data: any) {
+        console.log(data,'[data from h5 acceptDataFromOpenedPage]');
+      },
+      someEvent: function (data: any) {
+        console.log(data, '[data from h5 someEvent]');
+      },
+    },
+    success: function (res: any) {
+      console.log(res, '[res from h5]')
+      // 通过 eventChannel 向被打开页面传送数据
+      // res.eventChannel.emit("acceptDataFromOpenerPage", { data: "test" });
+    },
+  });
 
   // window.wx.miniProgram.navigateBack({delta: 1})
   // window.wx.miniProgram.redirectTo({
